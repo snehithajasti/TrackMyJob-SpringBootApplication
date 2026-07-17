035e1d967ddfc1fbf151a0814a503fd86e2af3f4
Snehitha <snehithajasti@gmail.com>
2026-07-17 22:03:20 +0530

    Initial project setup with Vite, Tailwind CSS, and project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-slate-100'>
+      <h1 className='text-5xl font-bold text-indigo-600'>Welcome to TrackMyJob</h1>
+    </div>
+  )
+}
+
+export default App
